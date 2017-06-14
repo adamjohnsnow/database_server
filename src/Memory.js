a4b6a98bcc88479d51bdeb3947a281accd36a7fd
Adam Snow <adamjohnsnow@icloud.com>
2017-06-14 10:43:25 +0100

    further memory testing

--- src/Memory.js
+++ src/Memory.js
@@ -6,8 +6,4 @@ Memory.prototype.saveRecord = function(key, value){
   this.database[key] = value;
 }
 
-Memory.prototype.findRecord = function(number){
-  return this.database[number - 1];
-}
-
 module.exports = Memory;
